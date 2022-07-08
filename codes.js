a69e3ee99c33acfe7b2102a5f62cf2294dc291ef
Marc Do <domarc251@gmail.com>
2022-07-08 13:41:25 +0200

    second commit heroku

---
+++ codes.js
@@ -0,0 +1,5 @@
+const keyMongodb =
+  "mongodb+srv://marc:marc@clustermoi-z4qk0.gcp.mongodb.net/morningnews2?retryWrites=true&w=majority";
+const keyApi = "13629796e4354c46a08ad6cd6798e25c";
+
+module.exports = { keyApi, keyMongodb };
