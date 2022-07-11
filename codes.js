8221d05cb8f9019d862e2a13c26e727cae543360
Marc <56676837+Do-moi@users.noreply.github.com>
2022-07-11 15:41:43 +0200

    Delete codes.js

--- codes.js
+++
@@ -1,5 +0,0 @@
-const keyMongodb =
-  "mongodb+srv://marc:marc@clustermoi-z4qk0.gcp.mongodb.net/morningnews2?retryWrites=true&w=majority";
-const keyApi = "13629796e4354c46a08ad6cd6798e25c";
-
-module.exports = { keyApi, keyMongodb };
